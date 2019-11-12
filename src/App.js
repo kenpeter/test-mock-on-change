97ebd77541827ed277d44802510f78ea35b809f4
Gary Liang <gary.liang@nab.com.au>
2019-11-12 14:09:20 +1100

    can do textarea

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/*
 import React from 'react';
 import styled from 'styled-components';
 
@@ -27,4 +28,37 @@ function App() {
   );
 }
 
+export default App;
+*/
+
+import React from 'react';
+export class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mirror: ''
+    };
+    this.updateMirror = this.updateMirror.bind(this);
+  }
+
+  updateMirror(e) {
+    let val = e.target.value;
+    this.setState((prevState, props) => {
+      return {
+        mirror: val
+      };
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <textarea type="text" onChange={this.updateMirror} />
+        </div>
+        <div test="mirror">{this.state.mirror}</div>
+      </div>
+    );
+  }
+}
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,34 @@
 import React from 'react';
-import {FuncTextArea} from './App';
-import {configure, mount} from 'enzyme';
+import {App} from './App';
+import {configure, mount, shallow} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({adapter: new Adapter()});
 
-it('test onchange mock', () => {
-  const onChange = jest.fn();
-  const component = mount(<FuncTextArea onChange={onChange} value="hi" />);
-  component.find('textarea').simulate('change');
-  expect(onChange).toBeCalledWith('hi');
+/*
+describe('input field', () => {
+  it('test input field', () => {
+    const onChange = jest.fn();
+    const props = {
+      value: 'Hello world',
+      onChange
+    };
+    const wrapper = mount(<input {...props} />);
+    wrapper.find('input').simulate('change', {
+      target: {
+        value: 'This is just for test'
+      }
+    });
+    expect(onChange).toHaveBeenCalledWith('This is just for test');
+  });
+});
+*/
+
+it('dsf', () => {
+  const state = {mirror: ''};
+  const wrapper = shallow(<App {...state} />);
+  wrapper
+    .find('textarea')
+    .simulate('change', {target: {value: 'Your new Value'}});
+  expect(wrapper.state('mirror')).toBe('Your new Value');
 });
